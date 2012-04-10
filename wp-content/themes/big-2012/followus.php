dc467e59e903175707c2c54fb552c2b8c2c31f55
Tom Carter <tomcarter259@gmail.com>
2012-04-10 16:18:21 +0100

    Styled follow-us icons on homepage

--- wp-content/themes/big-2012/followus.php
+++ wp-content/themes/big-2012/followus.php
@@ -1,23 +1,48 @@
 <div id="follow-us">
 	<h3>Follow Us</h3>
 	<hr />
-	<ul>
+	<ul class="sidebar-list left">
 		<li>
-		<a href="http://www.twitter.com/BristolIG" id="twitter">twitter</a>
+			<a href="http://www.twitter.com/BristolIG" id="twitter">
+				<div class="sidebar-item homepage">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/twitter-icon.png" />
+					Twitter
+				</div>
+			</a>
 		</li>
 		<li>
-		<a href="http://www.youtube.com/BristolIG" id="youtube">youtube</a>
+			<a href="http://www.facebook.com/BristolIG" id="facebook">
+				<div class="sidebar-item homepage">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/facebook-icon.png" />
+					Facebook
+				</div>
+			</a>
 		</li>
 		<li>
-		<a href="#" id="rss">rss</a>
+			<a href="http://www.plus.google.com/BristolIG" id="google+">
+				<div class="sidebar-item homepage">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/gplus-icon.png" />
+					Google+
+				</div>
+			</a>
 		</li>
 	</ul>
-	<ul>
+	<ul class="sidebar-list right">
 		<li>
-		<a href="http://www.facebook.com/BristolIG" id="facebook">facebook</a>
+			<a href="http://www.youtube.com/BristolIG" id="youtube">
+				<div class="sidebar-item homepage">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/youtube-icon.png" />
+					YouTube
+				</div>
+			</a>
 		</li>
 		<li>
-		<a href="http://www.plus.google.com/BristolIG" id="google-plus">google+</a>
+			<a href="#" id="rss">
+				<div class="sidebar-item homepage">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/rss-icon.png" />
+					RSS
+				</div>
+			</a>
 		</li>
 	</ul>
 </div><!-- #follow-us -->
