4a15de993b43d819489dc9f2615ab63bc04aaa96
Tom Carter <tomcarter259@gmail.com>
2012-05-02 16:11:43 +0100

    fixed slideshow images

--- slideshow.php
+++ slideshow.php
@@ -15,28 +15,28 @@
 	</div><!-- .slide -->
 
 	<div class="slide" id="fluid simulation" title="INTERACTIONS BETWEEN DEFORMABLE OBJECTS AND FLUIDS">
-		<a href="/projects/fluidsimulation"><img src="img/fluid_simulation.jpg" alt="Fluid Simulation" /></a>
+		<a href="/projects/fluidsimulation"><img src="<?php echo get_template_directory_uri(); ?>/img/fluid_simulation.jpg" alt="Fluid Simulation" /></a>
 		<div class="slide-text dark" style="top: 110px; left: 550px; text-align: center;">
 			<p>Title and description will appear here</p>
 		</div>
 	</div>
 
 	<div class="slide" id="brain-computer interaction" title="MONITORING YOUR MOOD FOR INCREASED PRODUCTIVITY">
-		<a href="/projects/bci"><img src="img/brain-computer_interaction.jpg" alt="Brain-Computer Interaction" /></a>
+		<a href="/projects/bci"><img src="<?php echo get_template_directory_uri(); ?>/img/brain-computer_interaction.jpg" alt="Brain-Computer Interaction" /></a>
 		<div class="slide-text light" style="top: 130px; left: 50px;">
 			<p>Title and description will appear here</p>
 		</div>
 	</div>
 
 	<div class="slide" id="vortex mk II" title="FULL 360&deg; 3D DISPLAY ">
-		<a href="/projects/voidsphere"><img src="img/vortex.jpg" alt="Vortex" /></a>
+		<a href="/projects/voidsphere"><img src="<?php echo get_template_directory_uri(); ?>/img/vortex.jpg" alt="Vortex" /></a>
 		<div class="slide-text dark" style="top: 90px; left: 50px;">
 			<p>Title and description will appear here</p>
 		</div>
 	</div>
 
 	<div class="slide" id="M.U.S.T.A.R.D." title="MULTI-USER SEE THROUGH AUGMENTED REALITY DISPLAY">
-		<a href="/projects/mustard"><img src="img/mustard.jpg" alt="MUSTARD" /></a>
+		<a href="/projects/mustard"><img src="<?php echo get_template_directory_uri(); ?>/img/mustard.jpg" alt="MUSTARD" /></a>
 		<div class="slide-text dark" style="top: 90px; left: 550px;">
 			<p>Title and description will appear here</p>
 		</div>
