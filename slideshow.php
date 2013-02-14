e643dfc20e7e158ab97eaafac856d195219d1045
tomcarter259 <tomcarter259@gmail.com>
2013-02-14 14:41:25 +0000

    Removed fluids and vortex from slideshow. Added PiVOT to slideshow.

--- slideshow.php
+++ slideshow.php
@@ -1,11 +1,32 @@
 <div id="slideshow" class="container">
 
+	<div class="slide" id="brain-computer interaction" title="MONITORING YOUR MOOD FOR INCREASED PRODUCTIVITY">
+		<a href="/projects/bci"><img src="<?php echo get_template_directory_uri(); ?>/img/brain-computer_interaction.jpg" alt="Brain-Computer Interaction" /></a>
+		<div class="slide-text light" style="top: 130px; left: 410px;">
+			<p>Title and description will appear here</p>
+		</div>
+	</div>
+
 	<div class="slide" id="chronotape" title="TANGIBLE TIMELINE FOR FAMILY HISTORY RESEARCH">
 		<a href="/projects/chronotape"><img src="<?php echo get_template_directory_uri(); ?>/img/chronotape.jpg" alt="chronotape" /></a>
 		<div class="slide-text light" style="top: 100px; left: 550px;">
 			<p>Title and description will appear here</p>
 		</div><!-- .slide-text -->
 	</div><!-- .slide -->
+
+	<div class="slide" id="M.U.S.T.A.R.D." title="MULTI-USER SEE THROUGH AUGMENTED REALITY DISPLAY">
+		<a href="/projects/mustard"><img src="<?php echo get_template_directory_uri(); ?>/img/mustard.jpg" alt="MUSTARD" /></a>
+		<div class="slide-text dark" style="top: 90px; left: 50px;">
+			<p>Title and description will appear here</p>
+		</div>
+	</div>
+
+	<div class="slide" id="PiVOT" title="MULTIVIEW TABLETOP WITH SHARED AND PERSONAL SPACES">
+		<a href="/projects/pivot"><img src="<?php echo get_template_directory_uri(); ?>/img/PiVOT.jpg" alt="PiVOT" /></a>
+		<div class="slide-text dark" style="top: 100px; left: 550px;">
+			<p>Title and description will appear here</p>
+		</div>
+	</div>
 
 	<div class="slide" id="ultrahaptics" title="HAPTIC FEEDBACK POWERED BY ULTRASOUND">
 		<a href="/projects/ultrahaptics"><img src="<?php echo get_template_directory_uri(); ?>/img/ultrahaptics.jpg" alt="ultrahaptics" /></a>
@@ -14,33 +35,6 @@
 		</div><!-- .slide-text -->
 	</div><!-- .slide -->
 
-	<div class="slide" id="fluid simulation" title="INTERACTIONS BETWEEN DEFORMABLE OBJECTS AND FLUIDS">
-		<a href="/projects/fluidsimulation"><img src="<?php echo get_template_directory_uri(); ?>/img/fluid_simulation.jpg" alt="Fluid Simulation" /></a>
-		<div class="slide-text dark" style="top: 110px; left: 50px;">
-			<p>Title and description will appear here</p>
-		</div>
-	</div>
-
-	<div class="slide" id="brain-computer interaction" title="MONITORING YOUR MOOD FOR INCREASED PRODUCTIVITY">
-		<a href="/projects/bci"><img src="<?php echo get_template_directory_uri(); ?>/img/brain-computer_interaction.jpg" alt="Brain-Computer Interaction" /></a>
-		<div class="slide-text light" style="top: 130px; left: 410px;">
-			<p>Title and description will appear here</p>
-		</div>
-	</div>
-
-	<div class="slide" id="vortex mk II" title="FULL 360&deg; 3D DISPLAY ">
-		<a href="/projects/voidsphere"><img src="<?php echo get_template_directory_uri(); ?>/img/vortex.jpg" alt="Vortex" /></a>
-		<div class="slide-text dark" style="top: 90px; left: 50px;">
-			<p>Title and description will appear here</p>
-		</div>
-	</div>
-
-	<div class="slide" id="M.U.S.T.A.R.D." title="MULTI-USER SEE THROUGH AUGMENTED REALITY DISPLAY">
-		<a href="/projects/mustard"><img src="<?php echo get_template_directory_uri(); ?>/img/mustard.jpg" alt="MUSTARD" /></a>
-		<div class="slide-text dark" style="top: 90px; left: 50px;">
-			<p>Title and description will appear here</p>
-		</div>
-	</div>
 
 
 
